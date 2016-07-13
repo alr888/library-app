7c2f0394dfb37aaf34807cf635d1fe9f27c91122
alr888 <ariel.l.reyes@gmail.com>
2016-07-13 21:21:04 +1200

    updated lesson 3 homework

--- app/models/contact.js
+++ app/models/contact.js
@@ -6,47 +6,8 @@ export default Model.extend({
   email: attr('string'),
   message: attr('string'),
   
-  isDisabled: true,
-  
-   //isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-
-   isValid: Ember.computed.match('email', /^.+@.+\..+$/),
-
-   
-   isLong: Ember.computed.gte('message.length',7),
-   
-   /*
-     actions: {
-
-    saveMessage() {
-      
-	  const email = this.get('email');
-      const message = this.get('message');
-	  
-      const newContact = this.store.createRecord('contact', { email: email, message: message });
-      newContact.save();
-	  
-	  alert(`Saving of the following message is in progress: ${this.get('message')}`);
-      this.set('responseMessage', `Thank you! Your  email address: ${this.get('emailAddress')} and message: ${this.get('message')} was sent.`);
-      
-	  
-	  
-	  //*Disabled for Lesson 4*this.set('emailAddress', '');
-	  //*Disabled for Lesson 4*this.set('message', '');
-    }
-  }
-   */
-
- 
-   //ALR isTemp: Ember.computed.and('isValid','isLong'),
-   
-   //contact.set('email', ''),
-   //.set('message', ''),
-
-   //isValid: Ember.computed.notEmpty('name'),
-  // isValid: Ember.computed.notEmpty('name'),
-   
-    //isDisabled: Ember.computed.not('isTemp'),  
-  
-  
+  //isValid: Ember.computed.notEmpty('name'),
+  isValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  isLong: Ember.computed.gte('message.length',7),
+    
 });
