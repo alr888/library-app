000ad1a6b2e751f3810c4e0b0c3e0dde7d529ad4
alr888 <ariel.l.reyes@gmail.com>
2016-07-23 21:29:09 +1200

    latest changes

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,65 +1,19 @@
 import Ember from 'ember';
-
-export default Ember.Route.extend({
 	
+export default Ember.Route.extend({
 
-   model() {
+  model() {
     return this.store.createRecord('contact');
   },
 
-   
   actions: {
 
-
-    routeaction:  function(email, message) {
-
-      const newContact = this.store.createRecord('contact', {
-        email: email,
-         message: message
-      });
-
-     newContact.save();
-     
-     alert('Saving of the following message is in progress:' + message ); 
-
-
-    /* 
-    newContact.save().then((response) => {
-        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
-        this.set('emailAddress', '');
-        this.set('message', '');
-      });
-    */
-
-
-   // saveContact(newContact) {
-    //  newContact.save().then(() => this.transitionTo('contact'));
-    //},
-
-    //alert('Saving of the following message is in progress: ' + message );
-    
-
-  
+    saveMessage(newContact) {
+      newContact.save();
+	  this.controller.set('responseMessage', 'The message is saved');
+	  this.transitionTo('index');
+	  this.transitionTo('contact');
     }
-  }
-
-/*
-  actions: {
-
-    saveInvitation() {
-      const email = this.get('emailAddress');
-
-      const newInvitation = this.store.createRecord('invitation', {
-        email: email
-      });
-
-      newInvitation.save().then((response) => {
-        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
-        this.set('emailAddress', '');
-      });
-
-    }
-  }
-	*/
-	
+  } 
 });
+
